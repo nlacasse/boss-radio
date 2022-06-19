de1d743932c02dfdafa81ca6cb00e086d1bf3697
Nicolas Lacasse <nicolas.lacasse@gmail.com>
2022-06-19 14:23:11 -0700

    Add favicons to head.

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -52,6 +52,10 @@ const tpl = `
 <html>
 	<head>
 		<title>FreqM0d</title>
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
 	</head>
 	<body>
 		<h1>{{.Station}}</h1>
